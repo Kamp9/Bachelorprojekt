a44c2a0fc1e6481ec7b1b835a70f2e3c2cb6737b
Troels Kamp Leskes <kamp9@hotmail.com>
2015-09-14 15:25:38 +0200

    Nu med testsfil

--- cholesky_factorization.py
+++ cholesky_factorization.py
@@ -1,7 +1,7 @@
 from numpy import *
 
-
-def positive_difinite(A):
+"""
+def is_pos_def2(A):
     (m, n) = A.shape
     if m != n:
         return False
@@ -10,14 +10,15 @@ def positive_difinite(A):
         if linalg.det(A) <= 0:
             return False
     return True
+"""
 
 
-def is_pos_def(x):
-    return all(linalg.eigvals(x) > 0)
+def is_pos_def(A):
+    return all(linalg.eigvals(A) > 0)
 
 
 def cholesky(A):
-#    if is_pos_def(A):
+    if is_pos_def(A):
         (m, n) = A.shape
         L = zeros((m, m))
         for i in range(m):
@@ -29,18 +30,6 @@ def cholesky(A):
                 A = A[1:, 1:] - L10[:, newaxis] * L10[newaxis, :]
                 (m, n) = A.shape
         return L
-#    else:
-#        raise ValueError('Matrix is not positive definite')
-
-real_matrix = array([(3.3821, 0.8784, 0.3613, -2.0349),
-                (0.8784, 2.0068, 0.5587, 0.1169),
-                (0.3613, 0.5587, 3.6656, 0.7807),
-                (-2.0349, 0.1169, 0.7807, 2.5397)])
-
-imag_matrix = array([[1.+0.j, 0.-2.j],
-                     [0.+2.j, 5.+0.j]])
-
-rand_matrix = random.random((2, 2))
-print rand_matrix
-print cholesky(rand_matrix)
+    else:
+        raise ValueError('Matrix is not positive definite')
 
---
+++ tests.py
@@ -0,0 +1,15 @@
+from numpy import *
+import cholesky_factorization as chol
+
+real_matrix = array([(3.3821, 0.8784, 0.3613, -2.0349),
+                     (0.8784, 2.0068, 0.5587, 0.1169),
+                     (0.3613, 0.5587, 3.6656, 0.7807),
+                     (-2.0349, 0.1169, 0.7807, 2.5397)])
+
+imag_matrix = array([[1.+0.j, 0.-2.j],
+                     [0.+2.j, 5.+0.j]])
+
+rand_matrix = random.random((2, 2))
+
+print chol.cholesky(imag_matrix)
+
