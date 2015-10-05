cf440ef19791ace804c46006beb44042d062b160
Troels Kamp Leskes <kamp9@hotmail.com>
2015-10-05 14:35:05 +0200

    LU Så skulle partial gerne virke igen nu med _swap funktion

--- lu_decomposition.py
+++ lu_decomposition.py
@@ -4,7 +4,7 @@ import substitution
 
 
 def lu_inplace(A):
-    (m, n) = A.shape
+    m, n = A.shape
     A = A.astype(np.float64)
     for k in range(m-1):
         A[k+1:, k] = (1.0 / A[k, k]) * A[k+1:, k]  # division med 0 er muligt
@@ -15,7 +15,7 @@ def lu_inplace(A):
 
 
 def lu_out_of_place(A):
-    (m, n) = A.shape
+    m, n = A.shape
     A = A.astype(np.float64)
     U = np.zeros((m, m))
     L = np.identity(m)
@@ -39,75 +39,89 @@ def in_place_solve(A, b):
     x = substitution.backward_substitution(U, z)
     return x
 
-"""
-def lu_partial_pivot(A):
-    (m, n) = A.shape
-    L = np.identity(m)
-    P = np.identity(m)  # kan være P = L
-    U = A.astype(np.float64)
-    for k in range(m):
-        pivot = k + np.absolute(U[k:, k]).argmax(axis=0)
-        if k != pivot:
-            temp = U[k, :].copy()  # kan vi undgå copy()?
-            U[k, :] = U[pivot, :]
-            U[pivot, :] = temp
-            temp = P[k, :].copy()  # kan vi undgå copy()?
-            P[k, :] = P[pivot, :]
-            P[pivot, :] = temp
-            if k >= 1:
-                temp = L[k, :k].copy()  # kan vi undgå copy()?
-                L[k, :k] = L[pivot, :k]
-                L[pivot, :k] = temp
-        L[k+1:, k] = (1.0 / U[k, k]) * U[k+1:, k]
-        U[k+1:, k+1:] = U[k+1:, k+1:] - L[k+1:, k, np.newaxis] * U[k, k+1:]
-    return P, L, np.triu(U)
-"""
 
-
-def _swap(A, m, k, pivot, upper):  # kan være ikke at tage m med videre er ligeså hurtigt
+def _swap_row_upper(A, m, k, pivot):  # kan være ikke at tage m med videre er ligeså hurtigt
     temp = np.empty(m)
-    if upper:
-        temp[k:] = A[k, k:]
-        A[k, k:] = A[pivot, k:]
-        A[pivot, k:] = temp[k:]
-    else:
-        temp[:k] = A[k, :k]
-        A[k, :k] = A[pivot, :k]
-        A[pivot, :k] = temp[:k]
+    temp[k:] = A[k, k:]
+    A[k, k:] = A[pivot, k:]
+    A[pivot, k:] = temp[k:]
+
+
+def _swap_row_lower(A, m, k, pivot):  # kan være ikke at tage m med videre er ligeså hurtigt
+    temp = np.empty(m)
+    temp[:k] = A[k, :k]
+    A[k, :k] = A[pivot, :k]
+    A[pivot, :k] = temp[:k]
+
+
+def _swap_col_upper(A, m, k, pivot):  # kan være ikke at tage m med videre er ligeså hurtigt
+    temp = np.empty(m)
+    temp[:k+1] = A[:k+1, k]
+    A[:k+1, k] = A[:k+1, pivot]
+    A[:k+1, pivot] = temp[:k+1]
+
+
+def _swap_col_lower(A, m, k, pivot):  # kan være ikke at tage m med videre er ligeså hurtigt
+    temp = np.empty(m)
+    temp[k+1:] = A[k+1:, k]
+    A[k+1:, k] = A[k+1:, pivot]
+    A[k+1:, pivot] = temp[k+1:]
+
+
+def _permute(P, L, U, m, k, pivot, pivoting):  # permute skal nok regne m ud via shape
+    if pivoting == 0:
+        if k != pivot:
+            _swap_row_lower(L, m, k, pivot)
+            _swap_row_upper(U, m, k, pivot)
+            _swap_row_upper(P, m, 0, pivot)
+    if pivoting == 1:
+        P, Q = P
+        x, y = pivot
+        if k != x:
+            _swap_row_lower(L, m, k, x)
+            _swap_row_upper(U, m, k, x)
+            _swap_row_upper(P, m, 0, x)
+        if k != y:
+            _swap_col_lower(L, m, k, y)
+            _swap_col_upper(U, m, k, y)
+            _swap_col_upper(Q, m, 0, y)
+
+"""
+        temp = U[k, :].copy()
+        U[k, :] = U[x, :]
+        U[x, :] = temp
+        temp = P[k, :].copy()
+        P[k, :] = P[x, :]
+        P[x, :] = temp
+        if k >= 1:
+            temp = L[k, :k].copy()
+            L[k, :k] = L[x, :k]
+            L[x, :k] = temp
+    if k != y:
+        temp = U[:, k].copy()
+        U[:, k] = U[:, y]
+        U[:, y] = temp
+        temp = Q[:, k].copy()
+        Q[:, k] = Q[:, y]
+        Q[:, y] = temp
+        if k >= 1:
+            temp = L[:k, k].copy()
+            L[:k, k] = L[:k, y]
+            L[:k, y] = temp
+"""
 
 
 def lu_partial_pivot(A):
     m, n = A.shape
     L = np.identity(m)
-    P = np.identity(m)  # kan være P = L
+    P = np.identity(m)
     U = A.astype(np.float64)
     for k in range(m):
         pivot = k + np.absolute(U[k:, k]).argmax(axis=0)
-        if k != pivot:
-            _swap(U, m, k, pivot, True)
-            _swap(P, m, 0, pivot, True)
-            if k >= 1:
-                _swap(L, m, k, pivot, False)
+        _permute(P, L, U, m, k, pivot, 0)
         L[k+1:, k] = (1.0 / U[k, k]) * U[k+1:, k]
         U[k+1:, k+1:] = U[k+1:, k+1:] - L[k+1:, k, np.newaxis] * U[k, k+1:]
     return P, L, np.triu(U)
-
-
-def partial_solve(A, b):
-    P, L, U = lu_partial_pivot(A)  # kan også være lu_out_of_place(A)
-    z = substitution.forward_substitution(L, b)
-    x = substitution.backward_substitution(U, z)
-    return x
-
-
-def _find_max(A):
-    (m, n) = A.shape
-    max_entry = (A[0, 0], 0, 0)
-    for i in range(m):
-        for j in range(n):
-            if A[i, j] > max_entry[0]:
-                max_entry = (A[i, j], i, j)
-    return max_entry[1], max_entry[2]
 
 
 def _permutate(P, Q, L, U, k, x, y):
@@ -136,16 +150,20 @@ def _permutate(P, Q, L, U, k, x, y):
     return P, Q, L, U
 
 
+def _maxpos(a):
+    return np.unravel_index(np.argmax(np.abs(a)), a.shape)
+
+
 def lu_complete_pivot(A):
-    (m, n) = A.shape
+    m, n = A.shape
     L = np.identity(m)
-    P = np.identity(m)  # kan være P = L
+    P = np.identity(m)
     Q = np.identity(m)
     U = A.astype(np.float64)
     for k in range(m):
-        x, y = _find_max(A[k:, k:])
+        x, y = _maxpos(A[k:, k:])
         x, y = x + k, y + k
-        P, Q, L, U = _permutate(P, Q, L, U, k, x, y)
+        _permute((P, Q), L, U, m, k, (x, y), 1)
         L[k+1:, k] = (1.0 / U[k, k]) * U[k+1:, k]
         U[k+1:, k+1:] = U[k+1:, k+1:] - L[k+1:, k, np.newaxis] * U[k, k+1:]
     return P, Q, L, np.triu(U)
--- tests.py
+++ tests.py
@@ -76,8 +76,15 @@ lalala = np.array([[2, 3, 4],
                    [4, 9, 5]])
 
 
-print lu_decomposition.lu_partial_pivot(rand_matrix2)[1] == sp.lu(rand_matrix2)[1]
-print lu_decomposition.lu_partial_pivot(rand_matrix2)[2] == sp.lu(rand_matrix2)[2]
+print rand_matrix2
 
-print lu_decomposition.lu_partial_pivot(rand_matrix2)[0]
-print sp.lu(rand_matrix2)[0]
+
+print lu_decomposition.lu_complete_pivot(rand_matrix2)[2]
+print sp.lu(rand_matrix2)[1]
+
+
+"""
+temp[:k+1] = A[:k+1, k]
+A[:k+1, k] = A[:pivot, k]
+A[:pivot, k] = temp[:k+1]
+"""
