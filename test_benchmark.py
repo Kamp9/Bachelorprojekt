f1ce75873296f955d8ff8bdcea05a4b07e31a2bf
Troels Kamp Leskes <kamp9@hotmail.com>
2015-10-20 07:55:58 +0200

    mere opdelt så vi har benchmarks, correctness og standard tests i hver sin fil

---
+++ test_benchmark.py
@@ -0,0 +1,13 @@
+import cProfile
+import numpy as np
+import scipy.linalg as sp
+import cholesky_decomposition
+import lu_decomposition
+import lu_decomposition2
+import solve
+
+rand_matrix = np.random.rand(1000, 1000)
+rand_col = np.random.rand(1000, 1)
+
+cProfile.run('sp.solve(rand_matrix, rand_col)')
+cProfile.run('solve.solve(rand_matrix, rand_col, 3)')
